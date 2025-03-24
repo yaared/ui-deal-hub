d8abd9eff1191970a8d02950828475efb701281c
orsonbells <yaaredtruneh@gmail.com>
2025-03-24 14:12:44 -0400

    fix base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spot-search-bar-deals/',  // Match your repo name
+  base: '/ui-deal-hub/',  // Match your repo name
 })
